e7585d3b6639d89958d44ddabad5534751dbb8bc
yury gilm <ygilm@bk.ru>
2024-03-21 19:20:00 +0300

    added new endpoints for habits

--- habits/urls.py
+++ habits/urls.py
@@ -1,7 +1,11 @@
 from habits.apps import HabitsConfig
 
+from django.urls import path
+from .views import HabitListCreateAPIView, HabitDetailAPIView
+
 app_name = HabitsConfig.name
 
 urlpatterns = [
-
+    path('habits/', HabitListCreateAPIView.as_view(), name='habit-list'),
+    path('habits/<int:pk>/', HabitDetailAPIView.as_view(), name='habit-detail'),
 ]

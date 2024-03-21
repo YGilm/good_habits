c53f6e176837899bcb3041c587c1417b15851c7e
yury gilm <ygilm@bk.ru>
2024-03-21 19:20:14 +0300

    added new views

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Habit
+from .paginators import HabitsPaginator
+from .serializers import HabitSerializer
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class HabitListCreateAPIView(ListCreateAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = HabitsPaginator
+
+    def get_queryset(self):
+        # Публичные привычки для всех, свои привычки для авторизованных
+        if self.request.query_params.get('public', '0') == '1':
+            return Habit.objects.filter(is_public=True)
+        return Habit.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class HabitDetailAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return Habit.objects.all()
